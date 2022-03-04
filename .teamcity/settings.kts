dc1c855759cad30427c930cb46da6c125ded3e14
P0NDER0SA <mikepond11@gmail.com>
2022-03-04 12:00:21 -0500

    adding import

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,6 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dockerCommand
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.schedule
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.ScheduleTrigger
 /*
 The settings script is an entry point for defining a TeamCity
 project hierarchy. The script should contain a single call to the
